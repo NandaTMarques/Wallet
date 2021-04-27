69d01f2bd831a0558134f500b8dd9234aa5ccb78
Fernanda PC <nandateixmarques@gmail.com>
2021-04-27 10:52:36 -0300

    adicionando mudanças

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { bindActionCreators } from 'redux';
+import { Creators as walletActions } from '../actions/wallet.action';
+import Header from '../components/Header';
+import FormExpense from '../components/FormExpense';
+import './wallet.css';
+import ExpensesTable from '../components/ExpensesTable';
+import EditExpense from '../components/editExpense';
 
 class Wallet extends React.Component {
+  componentDidMount() {
+    const { fetchCurrencies } = this.props;
+    fetchCurrencies();
+  }
+
   render() {
-    return <div>TrybeWallet</div>;
+    const { isExpenseEdit } = this.props;
+    return (
+      <div>
+        <Header />
+        {isExpenseEdit && <EditExpense />}
+        {!isExpenseEdit && <FormExpense />}
+        <ExpensesTable />
+      </div>
+    );
   }
 }
 
-export default Wallet;
+Wallet.propTypes = {
+  fetchCurrencies: PropTypes.func.isRequired,
+  isExpenseEdit: PropTypes.bool.isRequired,
+};
+
+function mapStateToProps({ wallet: { editExpense, isExpenseEdit } }) {
+  return { editExpense, isExpenseEdit };
+}
+
+const mapDispatchToProps = (dispatch) => bindActionCreators(walletActions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Wallet);
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,72 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import { Types } from '../actions/wallet.action';
+
+const initialState = {
+  currencies: [],
+  expenses: [],
+  idCount: 0,
+  editExpense: [],
+  isExpenseEdit: false,
+};
+
+const saveCurrencies = (state = initialState, action) => ({
+  ...state, currencies: action.payload,
+});
+
+const addExpense = (state = initialState, action) => {
+  const newExpense = {
+    id: state.idCount,
+    ...action.payload,
+  };
+  return {
+    ...state,
+    expenses: [...state.expenses, newExpense],
+    idCount: state.idCount + 1,
+  };
+};
+
+function removeExpense(expenses, id) {
+  return expenses.filter((expense) => expense.id !== id);
+}
+
+function selectExpense(expenses, id) {
+  return expenses.filter((expense) => expense.id === id);
+}
+
+const wallet = (state = initialState, action) => {
+  switch (action.type) {
+  case Types.SAVE_CURRENCIES:
+    return saveCurrencies(state, action);
+  case Types.ADD_EXPENSE:
+    return addExpense(state, action);
+  case Types.ADD_EXPENSE_WITH_CURRENCIES:
+    return {
+      ...state,
+      expenses:
+      [...state.expenses, { id: state.idCount, ...action.payload }],
+      idCount: state.idCount + 1,
+    };
+  case Types.REMOVE_EXPENSE:
+    return {
+      ...state, expenses: [...removeExpense(state.expenses, action.payload)],
+    };
+  case Types.SELECT_EXPENSE:
+    console.log(action.payload);
+    return {
+      ...state,
+      editExpense: [...selectExpense(state.expenses, action.payload)],
+      expenses: [...removeExpense(state.expenses, action.payload)],
+      isExpenseEdit: true,
+    };
+  case Types.EDIT_EXPENSE:
+    return {
+      ...state,
+      expenses: [...state.expenses, action.payload],
+      editExpense: [],
+      isExpenseEdit: false,
+    };
+  default:
+    return state;
+  }
+};
+
+export default wallet;
